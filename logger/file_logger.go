4c78feba9aaba80ec3a10070f7ddf455fa063965
zheng.li <zheng.li@immomo.com>
2020-12-24 17:44:17 +0800

    add logger

--- logger/file_logger.go
+++ logger/file_logger.go
@@ -1,105 +1,65 @@
 package logger
 
 import (
-	"errors"
 	"fmt"
-	"log"
-	"os"
-	"path"
-	"syscall"
 	"time"
 
 	"github.com/gozelus/zelus_rest/timez"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 )
 
 const (
 	dateFormat      = "2006-01-02"
-	timeFormat = "2006-01-02T15:04:05.000Z07"
+	timeFormat      = "2006-01-02T15:04:05.000Z07"
 	hoursPerDay     = 24
 	bufferSize      = 100
 	defaultDirMode  = 0755
 	defaultFileMode = 0600
 )
 
-type fileLogger struct {
-	keepDays int
-	path     string
-
+// 用于外部定制 options
+type Option func(o *options)
+type options struct {
 	// prefix 日志文件前缀
 	prefix string
-	// 当前正在写入的文件， fp -> 文件描述符
-	fp *os.File
-	// 当前正在写入的文件名
-	filename string
-	// 预先创建的文件名
-	backup string
+	// path  日志文件路径
+	path string
 
-	// 用于接收 write 方法写进来的字节
-	bytesChannel chan []byte
-	// 用于标志当前logger已经不接受写入
-	done chan int
+	// MaxAge 日志文件最大保存时间
+	MaxAge time.Duration
+	// RotationTime 分隔时间
+	RotationTime time.Duration
 }
 
-func newFileLogger(path, prefix string) (*fileLogger, error) {
-	f := &fileLogger{
+type fileLogger struct {
+	// 当前正在写入的文件， fp -> 文件描述符
+	fp      *rotatelogs.RotateLogs
+	options options
+}
+
+func newFileLogger(path, prefix string, opts ...Option) (*fileLogger, error) {
+	options := &options{
 		prefix:       prefix,
-		keepDays:     7,
 		path:         path,
-		fp:           nil,
-		filename:     fmt.Sprintf("%s/%s-%s.log", path, prefix, getNowDate()),
-		backup:       "",
-		bytesChannel: make(chan []byte),
+		MaxAge:       time.Hour * 24 * 10, // 默认保存10天
+		RotationTime: time.Hour * 24,      // 默认一天一分隔
 	}
-	if err := f.init(); err != nil {
+	for _, o := range opts {
+		o(options)
+	}
+	f := &fileLogger{
+		options: *options,
+	}
+
+	var err error
+	if f.fp, err = rotatelogs.New(fmt.Sprintf("%s/%s.%s.log", path, prefix, "%Y-%m-%d"), rotatelogs.WithMaxAge(f.options.MaxAge), rotatelogs.WithRotationTime(f.options.RotationTime)); err != nil {
 		return nil, err
 	}
-	f.startWorker()
 	return f, nil
 }
 
 func (f *fileLogger) Write(data []byte) (int, error) {
-	select {
-	case f.bytesChannel <- data:
-		return len(data), nil
-	case <-f.done:
-		log.Println(string(data))
-		return 0, errors.New("error: log file closed")
-	}
-}
-
-// 内部启动一个goroutine，接收channel中的字符并写入文件描述符内
-func (f *fileLogger) startWorker() {
-	go func() {
-		for {
-			select {
-			case event := <-f.bytesChannel:
-				f.fp.Write(event)
-			case <-f.done:
-				return
-			}
-		}
-	}()
-}
-
-// init 根据配置创建文件夹及文件
-func (f *fileLogger) init() error {
-	if _, err := os.Stat(f.filename); err != nil {
-		basePath := path.Dir(f.filename)
-		if _, err = os.Stat(basePath); err != nil {
-			if err = os.MkdirAll(basePath, defaultDirMode); err != nil {
-				return err
-			}
-		}
-		if f.fp, err = os.Create(f.filename); err != nil {
-			return err
-		}
-	} else if f.fp, err = os.OpenFile(f.filename, os.O_APPEND|os.O_WRONLY, defaultFileMode); err != nil {
-		return err
-	}
-
-	// 进程退出时关闭文件描述符?
-	syscall.CloseOnExec(int(f.fp.Fd()))
-	return nil
+	return f.fp.Write(data)
 }
 
 func getTimestamp() string {
