1ee0dc3803177096f9c88bcdd210451e952c5b6e
zheng.li <zheng.li@immomo.com>
2020-12-24 18:34:22 +0800

    add logger

--- logger/loggerz.go
+++ logger/loggerz.go
@@ -1,25 +1,49 @@
 package logger
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"runtime"
 	"sync"
+	"sync/atomic"
 
 	"github.com/fatih/color"
 )
 
+const (
+	skipStack = 4
+)
 var (
+	dunno     = []byte("???")
+	centerDot = []byte("·")
+	dot       = []byte(".")
+	slash     = []byte("/")
+)
+var (
+	// 控制台输出
 	console = os.Stdout
-	file    io.Writer
+	// 文件输出
+	file io.Writer
 
+	// 输出级别
 	logLevel LogLevel
-	mode     LogOutputMode
+
+	// 输出方式
+	mode LogOutputMode
+
+	// 控制台输出颜色控制，日志采集后带上颜色会导致读取错误，生产环境最好关掉
 	colorful bool
 
+	// 控制初始化次数
 	once sync.Once
+
+	// 是否初始化过的标志位，用原子操作读写
+	initialized uint32
 )
 
 type LogOutputMode uint32
@@ -75,8 +99,20 @@ type LogConf struct {
 	Colorful bool
 }
 
+func getDefaultConf() LogConf {
+	return LogConf{
+		ServiceName: "api",
+		Mode:        AllMode,
+		FilePath:    "/tmp/logs",
+		KeepDays:    10,
+		Level:       DebugLogLevel,
+		Colorful:    false,
+	}
+}
+
 func MustSetup(c LogConf) {
 	once.Do(func() {
+		atomic.StoreUint32(&initialized, 1)
 		setupLogLevel(&c)
 		setFilePath(&c)
 		setupMode(&c)
@@ -127,6 +163,30 @@ func setupWithFiles(c *LogConf) {
 	}
 }
 
+func InfoWithStack(v ...interface{}) {
+	infoWithStack(fmt.Sprint(v))
+}
+func DebugWithStack(v ...interface{}) {
+	debugWithStack(fmt.Sprint(v))
+}
+func WarnWithStack(v ...interface{}) {
+	warnWithStack(fmt.Sprint(v))
+}
+func ErrorWithStack(v ...interface{}) {
+	errorzWithStack(fmt.Sprint(v))
+}
+func InfofWithStack(str string, args ...interface{}) {
+	infoWithStack(fmt.Sprintf(str, args...))
+}
+func DebugfWithStack(str string, args ...interface{}) {
+	debugWithStack(fmt.Sprintf(str, args...))
+}
+func WarnfWithStack(str string, args ...interface{}) {
+	warnWithStack(fmt.Sprintf(str, args...))
+}
+func ErrorfWithStack(str string, args ...interface{}) {
+	errorzWithStack(fmt.Sprintf(str, args...))
+}
 func Info(v ...interface{}) {
 	info(fmt.Sprint(v))
 }
@@ -160,11 +220,22 @@ type logEntry struct {
 	Level     LogLevel        `json:"-"`
 }
 
+func warnWithStack(msg string) {
+	output(WarnLogLevel, formatWithCaller(msg, skipStack))
+}
+func errorzWithStack(msg string) {
+	output(ErrorLogLevel, formatWithCaller(msg, skipStack))
+}
+func debugWithStack(msg string) {
+	output(DebugLogLevel, formatWithCaller(msg, skipStack))
+}
+func infoWithStack(msg string) {
+	output(InfoLogLevel, formatWithCaller(msg, skipStack))
+}
 func warn(msg string) {
 	output(WarnLogLevel, msg)
 }
 func errorz(msg string) {
-	// TODO need add call stack
 	output(ErrorLogLevel, msg)
 }
 func debug(msg string) {
@@ -174,18 +245,10 @@ func info(msg string) {
 	output(InfoLogLevel, msg)
 }
 
-func getWriters() []io.Writer {
-	var writers []io.Writer
-	if (mode | FileMode) > 0 {
-		writers = append(writers, file)
-	}
-	if (mode | ConsoleMode) > 0 {
-		writers = append(writers, console)
-	}
-	return writers
-}
-
 func output(level LogLevel, msg string) {
+	if atomic.LoadUint32(&initialized) == 0 {
+		MustSetup(getDefaultConf())
+	}
 	shouldLog(level, func() {
 		entry := &logEntry{
 			Timestamp: getTimestamp(),
@@ -236,4 +299,68 @@ func outputJson(entry *logEntry, writer io.Writer) {
 		log.Fatal(err)
 	}
 	writer.Write(append(bytes, '\n'))
+}
+
+func formatWithCaller(msg string, skip int) string{
+	return fmt.Sprintf("%s\n\n%s", msg, stack(skip))
+}
+
+func stack(skip int) []byte {
+	buf := new(bytes.Buffer) // the returned data
+	// As we loop, we open files and read them. These variables record the currently
+	// loaded file.
+	var lines [][]byte
+	var lastFile string
+	for i := skip; ; i++ { // Skip the expected number of frames
+		pc, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		// Print this much at least.  If we can't find the source, it won't show.
+		fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
+		if file != lastFile {
+			data, err := ioutil.ReadFile(file)
+			if err != nil {
+				continue
+			}
+			lines = bytes.Split(data, []byte{'\n'})
+			lastFile = file
+		}
+		fmt.Fprintf(buf, "\t%s: %s\n", function(pc), source(lines, line))
+	}
+	return buf.Bytes()
+}
+
+// function returns, if possible, the name of the function containing the PC.
+func function(pc uintptr) []byte {
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return dunno
+	}
+	name := []byte(fn.Name())
+	// The name includes the path name to the package, which is unnecessary
+	// since the file name is already included.  Plus, it has center dots.
+	// That is, we see
+	//	runtime/debug.*T·ptrmethod
+	// and want
+	//	*T.ptrmethod
+	// Also the package path might contains dot (e.g. code.google.com/...),
+	// so first eliminate the path prefix
+	if lastSlash := bytes.LastIndex(name, slash); lastSlash >= 0 {
+		name = name[lastSlash+1:]
+	}
+	if period := bytes.Index(name, dot); period >= 0 {
+		name = name[period+1:]
+	}
+	name = bytes.Replace(name, centerDot, dot, -1)
+	return name
+}
+
+// source returns a space-trimmed slice of the n'th line.
+func source(lines [][]byte, n int) []byte {
+	n-- // in stack trace, lines are 1-indexed but our array is 0-indexed
+	if n < 0 || n >= len(lines) {
+		return dunno
+	}
+	return bytes.TrimSpace(lines[n])
 }
