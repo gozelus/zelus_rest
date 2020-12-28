05a9420cfb646d292817d56e3635eafe32e500a1
Rabbbit <13681320844@163.com>
2020-12-28 18:33:31 +0800

    sql 完善

--- core/sqlz/manager.go
+++ core/sqlz/manager.go
@@ -2,66 +2,11 @@ package sqlz
 
 import (
 	"database/sql"
-	"github.com/gozelus/zelus_rest"
 	"github.com/gozelus/zelus_rest/core/syncz"
 	"io"
 	"sync"
 	"time"
 )
-
-// Session 用于操作 MySQL 的接口
-type Session interface {
-	// Exec 执行一个 SQL
-	// example UPDATE INSERT DELETE
-	Exec(ctx rest.Context, query string, args ...interface{}) (*sql.Result, error)
-
-	// QueryRow 仅查询一行
-	// example SELECT
-	QueryRow(ctx rest.Context, v interface{}, query string, args ...interface{}) error
-
-	// QueryRows 查询多行
-	// example SELECT
-	QueryRows(ctx rest.Context, v interface{}, query string, args ...interface{}) error
-}
-
-type commonSqlConn struct {
-	driverName string
-	datasource string
-}
-
-func (db *commonSqlConn) QueryRow(ctx rest.Context, v interface{}, q string, args ...interface{}) error {
-	var conn *sql.DB
-	var err error
-	conn, err = getSqlConn(db.driverName, db.datasource)
-	if err != nil {
-		//logInstanceError(db.datasource, err)
-		return err
-	}
-	rows, err := queryContext(ctx, conn, q, args...)
-	if err != nil {
-		return err
-	}
-	return unmarshalRows(v, rows, false)
-}
-
-func (db *commonSqlConn) QueryRows(v interface{}, query string, args ...interface{}) error {
-	panic("implement me")
-}
-
-func (db *commonSqlConn) Exec(q string, args ...interface{}) (sql.Result, error) {
-	var conn *sql.DB
-	var err error
-	conn, err = getSqlConn(db.driverName, db.datasource)
-	if err != nil {
-		//logInstanceError(db.datasource, err)
-		return nil, err
-	}
-	return exec(conn, q, args...)
-}
-
-func NewCommonSqlConn() Session {
-	return &commonSqlConn{}
-}
 
 type pingedDB struct {
 	*sql.DB
