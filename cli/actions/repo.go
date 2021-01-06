e96df215b47eef3a1207656c18b30665cbef388f
Rabbbit <13681320844@163.com>
2021-01-07 00:27:32 +0800

    fix

--- cli/actions/repo.go
+++ cli/actions/repo.go
@@ -20,27 +20,29 @@ func GenRepo(ctx *cli.Context) error {
 	if _, err = mkdirIfNotExist("./internal/biz/repos"); err != nil {
 		return err
 	}
+	errorsFile, ex, err := createIfNotExist("./internal/biz/repos/errors.go")
+	if err != nil {
+		return err
+	}
+	if !ex {
+		r := codegen.NewRepoGener(nil, nil, "repos", moduleName)
+		if err := r.GenErrorsCode(errorsFile); err != nil {
+			return err
+		}
+	}
+
 	for _, table := range strings.Split(pattern, ",") {
-		m := codegen.NewPoModelStructInfo(table, url, "repos")
 		path := fmt.Sprintf("./internal/biz/repos/%s_repo.go", table)
 		file, ex, err := createIfNotExist(path)
 		if err != nil {
 			return nil
 		}
-		r := codegen.NewRepoGener(file, m, "repos", moduleName)
 		if ex {
 			fmt.Println(color.MagentaString("%s repo file exist , will ignore to write ... ", path))
 			continue
 		}
-		errorsFile, ex, err := createIfNotExist("./internal/biz/repos/errors.go")
-		if err != nil {
-			return err
-		}
-		if !ex {
-			if err := r.GenErrorsCode(errorsFile); err != nil {
-				return err
-			}
-		}
+		m := codegen.NewPoModelStructInfo(table, url, "repos")
+		r := codegen.NewRepoGener(file, m, "repos", moduleName)
 		if err = r.GenCode(); err != nil {
 			return err
 		}
