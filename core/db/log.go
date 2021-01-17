3bafd1825135e46e371019776231f78257ffa156
Rabbbit <13681320844@163.com>
2021-01-17 23:28:57 +0800

    fix

--- core/db/log.go
+++ core/db/log.go
@@ -12,8 +12,10 @@ func exec(ctx rest.Context, sql string, f func() (int64, error)) error {
 	now := time.Now()
 	rows, err := f()
 	consume := time.Now().Sub(now).Milliseconds()
-	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
-		logger.ErrorfWithContext(ctx, "[%dms] [rows:%d] FAIL SQL : `%s` error for %s", consume, rows, sql, err)
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			logger.ErrorfWithContext(ctx, "[%dms] [rows:%d] FAIL SQL : `%s` error for %s", consume, rows, sql, err)
+		}
 		return err
 	}
 	if consume <= 200 { // 200ms
