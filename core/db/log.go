8bd2bd4fc4e21525307ed17d3a88b62fd606e502
Rabbbit <13681320844@163.com>
2021-01-17 23:05:42 +0800

    fix

--- core/db/log.go
+++ core/db/log.go
@@ -1,17 +1,18 @@
 package db
 
 import (
+	"errors"
 	"github.com/gozelus/zelus_rest"
 	"github.com/gozelus/zelus_rest/logger"
+	"gorm.io/gorm"
 	"time"
 )
-
 
 func exec(ctx rest.Context, sql string, f func() (int64, error)) error {
 	now := time.Now()
 	rows, err := f()
 	consume := time.Now().Sub(now).Milliseconds()
-	if err != nil {
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 		logger.ErrorfWithContext(ctx, "[%dms] [rows:%d] FAIL SQL : `%s` error for %s", consume, rows, sql, err)
 		return err
 	}
